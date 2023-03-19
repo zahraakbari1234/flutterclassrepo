6efd2c181d99e2ecd03803cc027d857464571c0b
zahraakbari1234 <zhra.akbari@gmail.com>
2023-03-19 12:23:56 +0330

    complete

--- musicplayer/lib/main.dart
+++ musicplayer/lib/main.dart
@@ -7,24 +7,44 @@ void main() {
 
 class XylophoneApp extends StatelessWidget {
 
-  static AudioCache player = AudioCache();
 
   const XylophoneApp({Key? key}) : super(key: key);
 
+  void play ( int i){
+    AudioCache player = AudioCache();
+    player.play("note$i.wav");
+  }
+
+  Expanded buildKey({Color? color , int soundNumber = 1}){
+    return Expanded(
+      child: MaterialButton(
+        onPressed: (){
+          play(soundNumber);
+        },
+        color: color,
+        child: const Text("play your music"),
+      ),
+    );
+
+  }
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: InkWell(
-            onTap: (){
-              player.play("note1.wav");
-            },
-            child: Container(
-              height: 20,
-              color: Colors.amber,
-              child: Text("play your music"),
-            ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              buildKey(color: Colors.amber , soundNumber: 1),
+              buildKey(color: Colors.blue , soundNumber: 2),
+              buildKey(color: Colors.pink , soundNumber: 3),
+              buildKey(color: Colors.teal , soundNumber: 4),
+              buildKey(color: Colors.deepPurple , soundNumber: 5),
+              buildKey(color: Colors.orange , soundNumber: 6),
+              buildKey(color: Colors.lightGreen , soundNumber: 7),
+            ],
           ),
         ),
       ),
