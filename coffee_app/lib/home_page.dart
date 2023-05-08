69d0a7c99ac760c52d0f9ac37c0f6e1ae00912d6
zahraakbari1234 <zhra.akbari@gmail.com>
2023-05-08 20:45:55 +0330

    set resize false

--- coffee_app/lib/home_page.dart
+++ coffee_app/lib/home_page.dart
@@ -34,6 +34,7 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       backgroundColor: Colors.grey[900],
       appBar: AppBar(
         elevation: 0,
